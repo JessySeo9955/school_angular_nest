96d3f11d0cb20a5208ddd0f4a728bc006fa51987
JessySeo9955 <jessy.seo.9955@gmail.com>
2025-03-27 03:11:22 -0400

    development, production docker completed

--- start.sh
+++ start.sh
@@ -6,22 +6,12 @@ if [ -z "$environment" ] || [ "$environment" != 'production' ]; then
   environment='development'
 fi
 
-echo "=== Start ========"
 echo "==== START DOCKER-COMPOSE ====="
 if [  "$environment" == 'development' ];
 then
-  docker-compose -f docker-compose_${environment}.yml start
+  docker-compose -f docker-compose_${environment}.yml up
 else
   docker-compose -f docker-compose_${environment}.yml up
 fi
 echo "==== END DOCKER-COMPOSE ====="
 
-if [ "$environment" == 'development' ]; then
-  echo "==== START BACKEND ====="
-  # hot reload performance issue
-  cd ./back-end/school
-  npm run start:dev
-  echo "==== END BACKEND ====="
-fi
-
-echo "==== END ====="
